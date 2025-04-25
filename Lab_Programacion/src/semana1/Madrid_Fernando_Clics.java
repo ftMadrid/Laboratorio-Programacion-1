34be5cf7a1141dd91017095acc50cd2fea40d3f8
ftMadrid <fernandomadridcv04@gmail.com>
2025-04-25 15:53:08 -0600

    Created & Finished the Madrid_Fernando_Clics class
    Fully finished all the class.

--- Lab_Programacion/src/semana1/Madrid_Fernando_Clics.java
+++ Lab_Programacion/src/semana1/Madrid_Fernando_Clics.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package semana1;
 
-/**
- *
- * @author ricardomadrid
- */
+import java.util.Scanner;
+
 public class Madrid_Fernando_Clics {
     
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        double viente_clics = 20*0.80;
+        double sesenta_clics = 60*0.30;
+        double cien_clics = 100*0.25;
+        
+        double total = viente_clics+sesenta_clics+cien_clics;
+        double costo_promedio = total/180;
+        double isv = total*0.16;
+        double total_iva = total+isv;
+        
+        System.out.println("Total: "+total);
+        System.out.println(String.format("Costo promedio: %.2f", costo_promedio));
+        System.out.println("ISV: "+isv);
+        System.out.println("Total IVA: "+total_iva);  
+        
+    }
 }
