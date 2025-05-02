e68edae525ceeae7e9a50b04a9efbea3f8a0e057
ftMadrid <fernandomadridcv04@gmail.com>
2025-05-02 15:28:47 -0600

    Update Madrid_Fernando_Tiempo.java
    Finished all the class.

--- Lab_Programacion/src/semana2/Madrid_Fernando_Tiempo.java
+++ Lab_Programacion/src/semana2/Madrid_Fernando_Tiempo.java
@@ -1,5 +1,24 @@
 package semana2;
 
+import java.util.Scanner;
+
 public class Madrid_Fernando_Tiempo {
+    
+    public static void main(String[] args){
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        int horas, minutos, residuoHoras, residuoMinutos, nuevosSegundos;
+        
+        System.out.print("Ingresa un numero de segundos: ");
+        int segundos = entrada.nextInt();
+        
+        horas = segundos/3600;
+        residuoHoras = segundos % 3600;
+        minutos = residuoHoras/60;
+        nuevosSegundos = residuoHoras % 60;
+        
+        System.out.println(segundos > 0 ? "\n"+horas+" horas "+minutos+" minutos "+nuevosSegundos+" segundos" : "No se permite ese valor");
+    }
     
 }
