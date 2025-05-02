b9ea329c0115fd1611fdb6d7b2816142cd06116f
ftMadrid <fernandomadridcv04@gmail.com>
2025-05-02 16:13:25 -0600

    Update Madrid_Fernando_Fechas.java
    Finished all the class

--- Lab_Programacion/src/semana2/Madrid_Fernando_Fechas.java
+++ Lab_Programacion/src/semana2/Madrid_Fernando_Fechas.java
@@ -1,5 +1,36 @@
 package semana2;
 
+import java.util.Scanner;
+
 public class Madrid_Fernando_Fechas {
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Ingrese la primer fecha: (Formato: Dia/Mes/Anio)");
+        String primeraFecha = entrada.next();
+        System.out.println("Ingrese la segunda fecha: (Formato: Dia/Mes/Anio)");
+        String segundaFecha = entrada.next();
+        
+        String primerDias = primeraFecha.substring(0, 2);
+        String primerMes = primeraFecha.substring(3, 5);
+        String segundoDias = segundaFecha.substring(0, 2);
+        String segundoMes = segundaFecha.substring(3, 5);
+        
+        int calculoPrimeraFecha = (30 * Integer.parseInt(primerMes)) - (30 - Integer.parseInt(primerDias));
+        int calculoSegundaFecha = (30 * Integer.parseInt(segundoMes)) - (30 - Integer.parseInt(segundoDias));
+        
+        int total;
+        
+        if(calculoPrimeraFecha > calculoSegundaFecha) {
+            total = calculoPrimeraFecha - calculoSegundaFecha;
+            System.out.println("Diferencia de dias: "+total);
+        }else{
+            total = calculoSegundaFecha - calculoPrimeraFecha;
+            System.out.println("Diferencia de dias: "+total);
+        }
+        
+    }
     
 }
