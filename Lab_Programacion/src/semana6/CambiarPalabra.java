bdc21156922399d86b50a68f971113f468dc9823
ftMadrid <fernandomadridcv04@gmail.com>
2025-05-30 16:58:07 -0600

    Finished

--- Lab_Programacion/src/semana6/CambiarPalabra.java
+++ Lab_Programacion/src/semana6/CambiarPalabra.java
@@ -10,8 +10,13 @@ package semana6;
  */
 public class CambiarPalabra extends javax.swing.JFrame {
 
+    int posicion = 0;
+
     public CambiarPalabra() {
         initComponents();
+        labelTexto.setText("Ingrese la palabra #" + (posicion+1) + ":");
+        antiguas.setText("");
+        nuevas.setText("");
     }
 
     @SuppressWarnings("unchecked")
@@ -19,18 +24,37 @@ public class CambiarPalabra extends javax.swing.JFrame {
     private void initComponents() {
 
         tituloAhorcado = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        labelTexto = new javax.swing.JLabel();
         palabra = new javax.swing.JTextField();
         botonAceptar = new javax.swing.JButton();
+        volver = new javax.swing.JButton();
+        antiguas = new javax.swing.JLabel();
+        nuevas = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         tituloAhorcado.setFont(new java.awt.Font("Kefa", 1, 24)); // NOI18N
         tituloAhorcado.setText("Cambiar Palabras");
 
-        jLabel1.setText("Ingresa una palabra:");
+        labelTexto.setText("Ingresa una palabra:");
 
         botonAceptar.setText("Aceptar");
+        botonAceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonAceptarActionPerformed(evt);
+            }
+        });
+
+        volver.setText("Volver");
+        volver.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                volverActionPerformed(evt);
+            }
+        });
+
+        antiguas.setText("Antiguas");
+
+        nuevas.setText("Nuevas");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -39,12 +63,16 @@ public class CambiarPalabra extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(96, 96, 96)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
+                    .addComponent(nuevas)
+                    .addComponent(labelTexto)
                     .addComponent(tituloAhorcado)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(palabra, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(volver)
+                            .addComponent(palabra, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(botonAceptar)))
+                        .addComponent(botonAceptar))
+                    .addComponent(antiguas))
                 .addContainerGap(78, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -53,16 +81,64 @@ public class CambiarPalabra extends javax.swing.JFrame {
                 .addGap(32, 32, 32)
                 .addComponent(tituloAhorcado)
                 .addGap(39, 39, 39)
-                .addComponent(jLabel1)
+                .addComponent(labelTexto)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(palabra, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(botonAceptar))
-                .addContainerGap(154, Short.MAX_VALUE))
+                .addGap(27, 27, 27)
+                .addComponent(antiguas)
+                .addGap(18, 18, 18)
+                .addComponent(nuevas)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addComponent(volver)
+                .addGap(14, 14, 14))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void botonAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonAceptarActionPerformed
+        // TODO add your handling code here:
+
+        String[] palabrasAntiguas = Juego.palabras;
+        String antigua = "";
+        String nueva = "";
+        boolean chequeo = false;
+        Juego.palabras[posicion] = palabra.getText();
+        posicion++;
+
+        if (posicion > 9) {
+            botonAceptar.setEnabled(false);
+            palabra.setEnabled(false);
+            chequeo = true;
+        } else {
+            labelTexto.setText("Ingrese la palabra #" + (posicion+1) + ":");
+        }
+        
+        for(int i = 0; i<palabrasAntiguas.length;i++){
+            antigua += palabrasAntiguas[i]+" ";
+        }
+        for(int i = 0; i<Juego.palabras.length;i++){
+            nueva += Juego.palabras[i]+" ";
+        }
+        
+        if(chequeo){
+            antiguas.setText("Palabras Antiguas: "+antigua);
+            nuevas.setText("Palabras Nuevas: "+nueva);
+        }
+
+    }//GEN-LAST:event_botonAceptarActionPerformed
+
+    private void volverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_volverActionPerformed
+        // TODO add your handling code here:
+        Menu menu = new Menu();
+
+        dispose();
+        menu.setLocationRelativeTo(this);
+        menu.setVisible(true);
+
+    }//GEN-LAST:event_volverActionPerformed
 
     /**
      * @param args the command line arguments
@@ -100,9 +176,12 @@ public class CambiarPalabra extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel antiguas;
     private javax.swing.JButton botonAceptar;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel labelTexto;
+    private javax.swing.JLabel nuevas;
     private javax.swing.JTextField palabra;
     private javax.swing.JLabel tituloAhorcado;
+    private javax.swing.JButton volver;
     // End of variables declaration//GEN-END:variables
 }
