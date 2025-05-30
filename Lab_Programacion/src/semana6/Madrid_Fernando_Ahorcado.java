c18b5613e2b21c8288e5a5f90b09dbffc0c165ca
ftMadrid <fernandomadridcv04@gmail.com>
2025-05-30 15:55:57 -0600

    First Commit
    Uploaded files

---
+++ Lab_Programacion/src/semana6/Madrid_Fernando_Ahorcado.java
@@ -0,0 +1,13 @@
+package semana6;
+
+public class Madrid_Fernando_Ahorcado {
+    
+    public static void main(String[] args) {
+        
+        Menu menu = new Menu();
+        menu.setLocationRelativeTo(null);
+        menu.setVisible(true);
+        
+    }
+    
+}
